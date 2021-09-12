f1fa34407b21e295ebbc1cacdd9571b86b907d19
jailynreese <jreeset@gmail.com>
2021-09-11 22:15:17 -0400

    fix formatting

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,8 +1,57 @@
 const fs = require('fs'); // pull in hte file system module
 const path = require('path');
 
+function loadFile(request, response, filePath, contentType) {
+    const file = path.resolve(__dirname, filePath);
+
+    fs.stat(file, (err, stats) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                response.writeHead(404);
+            }
+            return response.end(err);
+        }
+
+        let { range } = request.headers;
+        if (!range) {
+            range = 'bytes=0-';
+        }
+
+        const positions = range.replace(/bytes=/, '').split('-');
+        let start = parseInt(positions[0], 10);
+
+        const total = stats.size;
+        const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+        if (start > end) {
+            start = end - 1;
+        }
+
+        const chunksize = (end - start) + 1;
+
+        response.writeHead(206, {
+            'Content-Range': `bytes ${start}-${end}/${total}`,
+            'Accepted-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': contentType,
+        });
+
+        const stream = fs.createReadStream(file, { start, end });
+
+        stream.on('open', () => {
+            stream.pipe(response);
+        });
+
+        stream.on('error', (streamErr) => {
+            response.end(streamErr);
+        });
+
+        return stream;
+    });
+}
+
 const getParty = (request, response) => {
-  loadFile(request, response, '../client/party.mp4', 'video/mp4');
+    loadFile(request, response, '../client/party.mp4', 'video/mp4');
 };
 
 const getBling = (request, response) => {
@@ -12,55 +61,6 @@ const getBling = (request, response) => {
 const getBird = (request, response) => {
     loadFile(request, response, '../client/bird.mp4', 'video/mp4');
 };
-
-function loadFile(request, response, filePath, contentType){
-    const file = path.resolve(__dirname, filePath);
-
-  fs.stat(file, (err, stats) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        response.writeHead(404);
-      }
-      return response.end(err);
-    }
-
-    let { range } = request.headers;
-    if (!range) {
-      range = 'bytes=0-';
-    }
-
-    const positions = range.replace(/bytes=/, '').split('-');
-    let start = parseInt(positions[0], 10);
-
-    const total = stats.size;
-    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-
-    if (start > end) {
-      start = end - 1;
-    }
-
-    const chunksize = (end - start) + 1;
-
-    response.writeHead(206, {
-      'Content-Range': `bytes ${start}-${end}/${total}`,
-      'Accepted-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': contentType,
-    });
-
-    const stream = fs.createReadStream(file, { start, end });
-
-    stream.on('open', () => {
-      stream.pipe(response);
-    });
-
-    stream.on('error', (streamErr) => {
-      response.end(streamErr);
-    });
-
-    return stream;
-  });
-}
 
 module.exports.getParty = getParty;
 module.exports.getBling = getBling;
