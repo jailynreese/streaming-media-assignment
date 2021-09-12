d4924c261ebcb233dbe34b6d06ae5a085061ae32
jailynreese <jreeset@gmail.com>
2021-09-11 22:06:31 -0400

    streaming media assignment

---
+++ src/htmlResponses.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const client2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+
+const getClient2 = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(client2);
+  response.end();
+};
+
+const client3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
+
+const getClient3 = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(client3);
+  response.end();
+};
+
+module.exports.getIndex = getIndex;
+module.exports.getClient2 = getClient2;
+module.exports.getClient3 = getClient3;
